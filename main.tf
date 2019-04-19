09b8da92627a09d62b51895d3b1219c467e9b7ce
rgustafs <rgustafsson@hashicorp.com>
2019-04-19 21:14:46 +0200

    first

---
+++ main.tf
@@ -0,0 +1,27 @@
+ provider "google" {
+}
+
+  resource "google_compute_instance" "demo" {
+  name         = "${var.instance_name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.gcp_zone}"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+}
+
+output "external_ip"{
+  value = "${google_compute_instance.demo.network_interface.0.access_config.0.assigned_nat_ip}"
+}
