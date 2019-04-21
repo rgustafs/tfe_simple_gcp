c03b7f0a063149026604724d257a1fe563b0796c
rgustafs <rgustafsson@hashicorp.com>
2019-04-21 21:12:52 +0200

    first commit

--- main.tf
+++ main.tf
@@ -14,14 +14,6 @@
 
   network_interface {
     network = "default"
-
-    access_config {
-      // Ephemeral IP
-    }
   }
 
 }
-
-#output "external_ip"{
-#  value = "${google_compute_instance.demo.network_interface.0.access_config.0.assigned_nat_ip}"
-#}
