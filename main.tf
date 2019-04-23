f1cb2ed157d99792f3ea99b3064a1ff80ac28e42
rgustafs <rgustafsson@hashicorp.com>
2019-04-23 22:58:44 +0200

    firewall

--- main.tf
+++ main.tf
@@ -22,4 +22,24 @@
   metadata {
   ssh-keys = "${var.ssh_user}:${var.ssh_pub_key}"
 }
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = "${google_compute_network.default.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80"]
+  }
+
+  source_tags = ["web"]
+}
+
+resource "google_compute_network" "default" {
+  name = "test-network"
+}
 }
